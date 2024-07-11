f0bebec2ae70a3fc5d594ac954a7c687e06d7b46
Abeeb Raheem <belovetech@gmail.com>
2024-07-11 19:13:35 +0100

    (feat): optimize the organizer to use goroutine

--- organizer/organizer.go
+++ organizer/organizer.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/belovetech/file-organizer/utils"
 )
@@ -21,47 +22,20 @@ func Organize(directory string, dryRun bool, verbose bool) error {
 		return fmt.Errorf("error reading directory: %v", err)
 	}
 
+	var wg sync.WaitGroup
 	for _, file := range files {
 
 		if file.IsDir() {
-			// logger.Info("Found directory", slog.String("directory", file.Name()))
+			logger.Info("Found directory", slog.String("directory", file.Name()))
 			continue
 			//Todo: Implement recursive directory search
 		}
 
-		fileExt := getExt(file)
-		newDir := filepath.Join(directory, fileExt)
-		oldFile := filepath.Join(directory, file.Name())
-		newFile := filepath.Join(newDir, file.Name())
-
-		if verbose {
-			logger.Info("Verbose: Processing file", slog.String("file", file.Name()), slog.String("directory", newDir))
-			return nil
-		}
-
-		if dryRun {
-			logger.Info("Dry run: Would have created directory", slog.String("directory", newDir))
-			logger.Info("Dry run: Would have moved file", slog.String("file", file.Name()), slog.String("directory", newDir))
-			return nil
-		}
-
-		if _, err := os.Stat(newDir); os.IsNotExist(err) {
-			err := os.Mkdir(newDir, 0755)
-			if err != nil {
-				logger.Error("Error creating directory", slog.Any("error", err))
-				return fmt.Errorf("error creating directory: %v", err)
-			}
-			logger.Info("Created directory", slog.String("directory", newDir))
-		}
-
-		e := os.Rename(oldFile, newFile)
-		if e != nil {
-			logger.Error("Error moving file", slog.Any("error", err))
-		}
-		logger.Info("Moved file", slog.String("file", file.Name()), slog.String("directory", newDir))
-
+		wg.Add(1)
+		go moveFile(directory, file, dryRun, verbose, &wg)
 	}
 	logger.Info("Finished organizing files")
+	wg.Wait()
 	return nil
 }
 
@@ -86,4 +60,40 @@ func getExt(file fs.DirEntry) string {
 		fileExt = fileExt[1:]
 	}
 	return fileExt
+}
+
+func moveFile(directory string, file fs.DirEntry, dryRun bool, verbose bool, wg *sync.WaitGroup) error {
+	defer wg.Done()
+	logger := utils.Log()
+
+	fileExt := getExt(file)
+	newDir := filepath.Join(directory, fileExt)
+	oldFile := filepath.Join(directory, file.Name())
+	newFile := filepath.Join(newDir, file.Name())
+
+	if verbose {
+		logger.Info("Verbose: Processing file", slog.String("file", file.Name()), slog.String("directory", newDir))
+		return nil
+	}
+
+	if dryRun {
+		logger.Info("Dry run: Would have created directory", slog.String("directory", newDir))
+		logger.Info("Dry run: Would have moved file", slog.String("file", file.Name()), slog.String("directory", newDir))
+		return nil
+	}
+	if _, err := os.Stat(newDir); os.IsNotExist(err) {
+		err := os.Mkdir(newDir, 0755)
+		if err != nil {
+			logger.Error("Error creating directory", slog.Any("error", err))
+			return fmt.Errorf("error creating directory: %v", err)
+		}
+		logger.Info("Created directory", slog.String("directory", newDir))
+	}
+
+	err := os.Rename(oldFile, newFile)
+	if err != nil {
+		logger.Error("Error moving file", slog.Any("error", err))
+	}
+	logger.Info("Moved file", slog.String("file", file.Name()), slog.String("directory", newDir))
+	return nil
 }
